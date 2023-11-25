83e05241d265d56dc845b9306b6ede88b7772367
Ramazan Sengul <ramazansengul137@gmail.com>
2023-11-25 14:31:41 +0300

    add compose libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,13 +36,20 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 }
 
 val paging_version = "3.2.1"
 val room_version = "2.6.0"
+val navVersion = "2.7.5"
 
 
 dependencies {
@@ -63,6 +70,21 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.4")
 
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:$navVersion")
+
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha10")
+    implementation("androidx.compose.material:material-icons-core")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
     implementation ("com.google.dagger:hilt-android:2.48")
     ksp ("com.google.dagger:hilt-compiler:2.48")
     ksp ("androidx.hilt:hilt-compiler:1.0.0")
@@ -75,6 +97,7 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    implementation("androidx.paging:paging-compose:3.2.1")
     implementation("androidx.paging:paging-runtime-ktx:$paging_version")
     implementation("androidx.room:room-paging:$room_version")
     implementation("androidx.room:room-runtime:$room_version")
